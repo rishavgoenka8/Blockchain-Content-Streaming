5027827b92190d7e1e7d83873c06562c4c7a38ce
Rishav Goenka <rishavgoenka8@gmail.com>
2023-04-12 19:27:50 +0530

    project done

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ModalProvider } from "react-modal-hook";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <ModalProvider>
+  //     <App />
+  //   </ModalProvider>
+  // </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
